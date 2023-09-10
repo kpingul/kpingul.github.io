8f5d70ec3947c66166ce6800958108ca84b628c4
kpingul <kirckpingul@gmail.com>
2023-09-10 12:21:59 -0700

    update blog order

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
 (function () {
     
     let blogs = [
-        {title: "AppSecCon: Application Security Conference", description: "Attending the virual security conference.", link: "blog/appseccon2023.html"},
-        {title: "NahamCon: Offensive Security Conference", description: "Attending the virual security conference.", link: "blog/nahamcon2023.html"},
+
         {title: "Tripwire: Leveraging Adversarial Tactics", description: "How learning from an attacker's tactics, teechniques, and procedures (TTPs) can help security teams.", link: "blog/tripwire.html"},
         {title: "Dependalyzer: Understanding Your Software Supply Chain", description: "Expanding on GitHub's Dependabot to help teams tackle software supply chain security.", link: "blog/dependalyzer.html"},
         {title: "BSides San Francisco: Infosec Conference", description: "Attending the BSides event.", link: "blog/bsidessf2023.html"},
-        {title: "What is Endpoint Security?", description: "Learn the importance of endpoint security.", link: "blog/endpointsecurity.html"},
         {title: "Basics of Threat Modeling", description: "Learn how threat modeling can help identify potential security threats.", link: "blog/threatmodel.html"},
+        {title: "AppSecCon: Application Security Conference", description: "Attending the virual security conference.", link: "blog/appseccon2023.html"},
+        {title: "NahamCon: Offensive Security Conference", description: "Attending the virual security conference.", link: "blog/nahamcon2023.html"},
+        {title: "What is Endpoint Security?", description: "Learn the importance of endpoint security.", link: "blog/endpointsecurity.html"},
         {title: "Intro to Packet Sniffing", description: "Learn about packet sniffing and it's importance", link: "blog/packetsniffer.html"},
         {title: "Intro to Burp Suite", description: "Learn about an essential tool for web application security testing.", link: "blog/burpsuite.html"},
         {title: "SVUS Awards", description: "Attending my first awards on behalf of my previous employer.", link: "blog/svusawards2019.html"},
